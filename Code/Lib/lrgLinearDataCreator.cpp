cad509ec62570a566a22a0035964a3dd8fe80742
Griffin Bassman <griffinbassman@Griffins-MacBook-Pro.local>
2020-03-04 16:43:26 +0000

    Add functionality to set theta0 and theta1 in constructor and add logic to generate y from x

--- Code/Lib/lrgLinearDataCreator.cpp
+++ Code/Lib/lrgLinearDataCreator.cpp
@@ -1,9 +1,29 @@
 #include "lrgLinearDataCreator.h"
 #include<iostream>
+#include<random>
 
-DataCreator::DataCreator() {}
+DataCreator::DataCreator(double t0, double t1) {
+	theta0 = t0;
+	theta1 = t1;
+}
 
 vector<pair <double, double> > DataCreator::GetData() {
-	cout << "GetData TEST OUTPUT\n";
-	return vector<pair <double, double> >(); 
+	// Set up random generator with x distribution from 0 to 0
+	// and the noise distribution from -1 to 1.
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> x_dist(0,10);
+	std::uniform_real_distribution<double> noise_dist(-1,1);
+	int n_points = 100; // Set number of generated points
+	vector<pair <double, double> > data_points; // Initialize return vector
+	// Loop through and generate a new x and noise point. Use these
+	// to calculate y, along with theta1 and theta0. Append the results
+	// the the result vector.
+	for(int i = 0; i < n_points; i++) {
+		double x = x_dist(generator);
+		double noise = noise_dist(generator);
+		double y = theta1*x + theta0 + noise;
+		pair <double,double> p(x, y);
+		data_points.push_back(p);
+	}
+	return data_points; 
 }
