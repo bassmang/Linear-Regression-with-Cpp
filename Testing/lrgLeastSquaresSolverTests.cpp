76add9d8a5f4a2c6556ab36fbaeef0d118836e1f
Griffin Bassman <griffinbassman@Griffins-MacBook-Pro.local>
2020-03-04 17:34:07 +0000

    Test data generation function and update initial function for new constructor

--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -2,9 +2,41 @@
 #include "lrgLinearDataCreator.h"
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-TEST_CASE( "Instantiate an instance of your new concrete class","[some group identifier]" ) {
-	DataCreator d;
-	d.GetData();
+TEST_CASE( "Test instantiate an instance of your new concrete class" ) {
+	DataCreator d(1, 1, 1);
 	REQUIRE( 1 == 1 );
+}
+
+TEST_CASE( "Test number and distribution from GetData function" ) {
+	// Set random values for theta0, theta1, and number of points
+	double theta0 = 50.0;
+	double theta1 = 0.5;
+	double n_points = 1000000;
+	// Generate data
+	DataCreator d(theta0, theta1, n_points);
+	vector<pair <double, double> > points = d.GetData();
+	// Variables to store total sum over x and y
+	double x_sum = 0;
+	double y_sum = 0;
+	// Loop though to get sum of x and y
+	for (int i = 0; i < n_points; i++) {
+		x_sum += points[i].first;
+		y_sum += points[i].second;
+	}
+	// Define expected values of x and y. In my function, x draws randomly
+	// from the range 0 to 10, thus the average value is 5. The exectped
+	// value of y is applies the linear function to the expected value of x.
+	// Note that I set the noise to be picked uniformly between -1 and 1, thus
+	// these should cancel out in the expectation of y
+	double x_exp = 5.0;
+	double y_exp = theta1*x_exp + theta0;
+	// Compute the x and y error as their average minus their expected value, then
+	// take the absolute value of that
+	double x_err = abs(x_exp - (x_sum / n_points));
+	double y_err = abs(y_exp - (y_sum / n_points));
+	// Require both errors to be small to ensure function works properly
+	REQUIRE( x_err < .001 );
+	REQUIRE( y_err < .001 );
 }
